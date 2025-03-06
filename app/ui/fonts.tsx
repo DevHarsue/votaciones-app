0cf019c6cf288b1f19ef29fcbacdcaa4521dd541
steven <stevenchencp41@gmail.com>
2025-03-06 19:34:24 -0400

    Creacion-pagina-de-inscripcion (#3)
    * creacion de pagina de inscripcion
    
    * arreglo de detalles de inscripcion

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,7 +1,8 @@
 import { Lusitana, Montserrat} from 'next/font/google'
 
 export const montserrat= Montserrat({
-subsets: ['latin'] })
+subsets: ['latin'] 
+})
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
