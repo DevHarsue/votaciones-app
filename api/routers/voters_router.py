2cc89b5b65ad862e1f6ee027371b4f59a2c1e24c
DevHarsue <99783227+DevHarsue@users.noreply.github.com>
2025-03-09 11:20:02 -0400

    Validacion para evitar multiples registros de votantes (#8)

--- api/routers/voters_router.py
+++ api/routers/voters_router.py
@@ -7,9 +7,17 @@ from ..actions.code_voter_actions import CodeVoterActions
 from .token import list_dependencies
 from ..utils.depends import depend_data
 from ..utils.emails import Email
-from ..utils.security import generate_code,verify_token
+from ..utils.security import generate_code
 
 voters_router = APIRouter()
+
+def send_code(id,receiver):
+    code = generate_code()
+    code_actions = CodeVoterActions()
+    if not code_actions.create_code(voter_id=id,code=code):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Code not keeped")
+    sender = Email()
+    sender.send_email_validate_voter(receiver,code)
 
 @voters_router.get("/get_voters",status_code=status.HTTP_200_OK,dependencies=list_dependencies)
 def get_voters(data: depend_data) -> List[VoterResponse]:
@@ -23,22 +31,38 @@ def get_voters(data: depend_data) -> List[VoterResponse]:
 @voters_router.post("/create_voter",status_code=status.HTTP_201_CREATED)
 def create_voter(voter: VoterRequest) -> VoterResponse:
     actions = VoterActions()
+    voter_exists = actions.get_voter(email=voter.email)
+    if voter_exists:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Email already exists")
+    
+    voter_exists = actions.get_voter_by_ci(nationality=voter.nationality,ci=voter.ci)
+    if voter_exists:
+        voter_response = actions.update_voter(
+            id=voter_exists.id,
+            voter=VoterUpdate(
+                email=voter.email
+            )
+        )
+        
+        if not voter_response:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Error creating voter")
+        
+        send_code(voter_response.id,voter_response.email)
+        
+        return JSONResponse(content=voter_response.model_dump(),
+                        status_code=status.HTTP_201_CREATED)
+        
     voter = actions.create_voter(voter)
-    
-    code = generate_code()
-    code_actions = CodeVoterActions()
-    if not code_actions.create_code(voter_id=voter.id,code=code):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Code not keeped")
-    
-    sender = Email()
-    sender.send_email_validate_voter(voter.email,code)
     if not voter:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Error creating voter")
+    
+    send_code(voter.id,voter.email)
+    
     return JSONResponse(content=voter.model_dump(),
                         status_code=status.HTTP_201_CREATED)
 
 @voters_router.get("/validate_voter/{code}",status_code=status.HTTP_200_OK)
-def validate_voter(code: int) -> VoterResponse:
+def validate_voter(code: int) -> JSONResponse:
     code_actions = CodeVoterActions()
     id = code_actions.validate_code(code)
     if id==0:
@@ -50,22 +74,18 @@ def validate_voter(code: int) -> VoterResponse:
     
     code_actions.delete_code(code=code,voter_id=id)
     
-    return JSONResponse(content={"message":"User validated"},
+    return JSONResponse(content={"message":"Voter validated"},
                         status_code=status.HTTP_200_OK)
 
 @voters_router.put("/update_voter",status_code=status.HTTP_200_OK,dependencies=list_dependencies)
 def update_voter(voter: VoterUpdate,id: int=Query(gt=0)) -> VoterResponse:
     actions = VoterActions()
     voter = actions.update_voter(id,voter)
-    code = generate_code()
-    code_actions = CodeVoterActions()
-    if not code_actions.create_code(voter_id=voter.id,code=code):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Code not keeped")
-    
-    sender = Email()
-    sender.send_email_validate_voter(voter.email,code)
     if not voter:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Error updating voter")
+    
+    send_code(voter.id,voter.email)
+    
     return JSONResponse(content=voter.model_dump(),
                         status_code=status.HTTP_200_OK)
 
