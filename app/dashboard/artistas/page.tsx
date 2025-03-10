0c6c86c853bb2959a1679804ff6abc22845602f8
Steukchen <stevenchencp41@gmail.com>
2025-03-10 08:03:42 -0400

    Creacion de paginas update para artistas, usuarios y votantes

--- app/dashboard/artistas/page.tsx
+++ app/dashboard/artistas/page.tsx
@@ -1,50 +1,37 @@
 'use client';
 import Link from "next/link"
 import DataRow from "../../ui/components/dataRow";
-import { NormalButton } from "../../ui/components/buttons";
+import { NormalButton} from "../../ui/components/buttons";
+import { useRouter } from "next/navigation";
 
-const artistas = [
-  { id: 1, name: "Artista 1" },
-  { id: 2, name: "Artista 2" },
-  { id: 3, name: "Artista 3" },
-];
+export default function ArtistaPage() {
+  const router = useRouter();
 
-export default function ArtistasPage() {
-  const handleEdit = (id: number) => {
-    console.log(`Modificar artista con ID: ${id}`);
-  };
-
-  const handleDelete = (id: number) => {
-    console.log(`Eliminar artista con ID: ${id}`);
+  const handleEditArtista = () => {
+    router.push("artistas/updateArtista");
   };
 
   return (
-    <main>
-    <div className="p-4 flex flex-col px-40">
-      <h2 className="text-2xl font-bold mb-4 text-center">
+   <main>
+    <div className="p-4 flex flex-col px-40"> 
+    <h2 className="text-2xl font-bold mb-4 text-center">
         Gestión de Artistas
       </h2>
-      {artistas.map((artista) => (
         <DataRow
-          key={artista.id}
-          name={artista.name}
-          onEdit={() => handleEdit(artista.id)}
-          onDelete={() => handleDelete(artista.id)}
+        name="Usuario Ejemplo"
+        onEdit={handleEditArtista}
+        onDelete={() => console.log("Eliminar usuario")}
         />
-      ))}
     </div>
-    {/* Botón: Volver a Inicio */}
-    <div className='mt-5'>
-            <Link href="/">
-                <NormalButton
-                    text='Volver a Inicio'
-                    color='text-blue-600'
-                    hoverClass='hover:text-blue-400'
-                    extraClass='w-full py-2 px-4 rounded-md md:w-full transition-colors'
-                    type='button'
+    <Link href="/">
+        <NormalButton
+                text='Volver a Inicio'
+                color='text-blue-600'
+                hoverClass='hover:text-blue-400'
+                extraClass='w-full py-2 px-4 rounded-md md:w-full transition-colors'
+                type='button'
                 />
             </Link>
-        </div>  
     </main>
   );
 }
--- app/dashboard/votantes/page.tsx
+++ app/dashboard/votantes/page.tsx
@@ -2,46 +2,35 @@
 import Link from "next/link"
 import DataRow from "../../ui/components/dataRow";
 import { NormalButton} from "../../ui/components/buttons";
+import { useRouter } from "next/navigation";
 
-const votantes = [
-    { id: 1, name: "Votante 1" },
-    { id: 2, name: "Votante 2" },
-    { id: 3, name: "Votante 3" },
-  ];
-  
-  export default function VotantesPage() {
-    const handleEdit = (id: number) => {
-      console.log(`Modificar votante con ID: ${id}`);
-    };
-  
-    const handleDelete = (id: number) => {
-      console.log(`Eliminar votante con ID: ${id}`);
-    };
-  
-    return (
-    <main>
-      <div className="p-4 flex flex-col px-40">
-        <h2 className="text-2xl font-bold mb-4 text-center">
-            Gestión de Votantes
-        </h2>
-        {votantes.map((votante) => (
-          <DataRow
-            key={votante.id}
-            name={votante.name}
-            onEdit={() => handleEdit(votante.id)}
-            onDelete={() => handleDelete(votante.id)}
-          />
-        ))}
-      </div>
-        <Link href="/">
-            <NormalButton
+export default function VotantesPage() {
+  const router = useRouter();
+
+  const handleEditVotante = () => {
+    router.push("votantes/updateVotante");
+  };
+  return (
+   <main>
+    <div className="p-4 flex flex-col px-40">
+      <h2 className="text-2xl font-bold mb-4 text-center">
+        Gestión de Votantes
+      </h2>
+        <DataRow
+        name="Usuario Ejemplo"
+        onEdit={handleEditVotante}
+        onDelete={() => console.log("Eliminar usuario")}
+        />
+    </div>
+    <Link href="/">
+        <NormalButton
                 text='Volver a Inicio'
                 color='text-blue-600'
                 hoverClass='hover:text-blue-400'
                 extraClass='w-full py-2 px-4 rounded-md md:w-full transition-colors'
                 type='button'
                 />
-        </Link>
+            </Link>
     </main>
-    );
-  }
+  );
+}
