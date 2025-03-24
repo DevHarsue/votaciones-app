f3d3bfa9df33c21d69cbeac4ec0cf61d0d301113
DevHarsue <99783227+DevHarsue@users.noreply.github.com>
2025-03-24 00:50:20 -0400

    creacion de interfaz de registro de usuarios y demás (#23)
    Co-authored-by: Steukchen <stevenchencp41@gmail.com>

--- app/ui/components/buttons.tsx
+++ app/ui/components/buttons.tsx
@@ -2,13 +2,14 @@
 
 import { ReactNode } from "react";
 export function NormalButton(
-{ text, color, hoverClass, extraClass = "", type = "button", onClick }:
+{ text, color, hoverClass, extraClass = "", type = "button", disabled = false, onClick }:
 { 
     text: string | ReactNode,
     color: string, 
     hoverClass: string, 
     extraClass?: string, 
     type?: "button" | "submit" | "reset",
+    disabled?: boolean
     onClick?: () => void; // Prop onClick opcional
 }
 ) {
@@ -17,6 +18,7 @@ export function NormalButton(
         type={type}
         className={`${color} py-2 px-4 rounded md:w-auto ${hoverClass} transition-colors ${extraClass}`}
         onClick={onClick} // onClick se pasa al botón
+        disabled={disabled}
         >
         {text}
         </button>
