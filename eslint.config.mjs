9dd1ee34c8110794a7c52964ad82c65e3a03ad2f
Harsue <harwingmr@gmail.com>
2025-04-19 22:48:36 -0400

    Cambio en el lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'jsx-a11y/alt-text': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
